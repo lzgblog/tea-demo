4b3cd3c8ddbcb704ab761b81a3ea874916da7786
lzgblog <1558095899@qq.com>
2024-03-06 17:23:57 +0800

    11

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const figlet = require('figlet');
 const teaart = require('tea-word-art');
 
+const teaart1 = require('tea-character-graph');
+const teaart2 = require('tea-classification-description');
+const teaart3 = require('tea-introduce');
+const teaart4 = require('check-ip-util');
+const teaart5 = require('substring-by-length');
 
 function printTea() {
 	figlet('Tea Time!', function(err, data) {
